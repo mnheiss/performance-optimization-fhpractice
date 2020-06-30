7ebb7811071171ad5d5fb334f4136a6b8d00d701
Melanie Heiss <1910738178@stud.fh-kufstein.ac.at>
2020-06-30 21:36:45 +0200

    PWA changes

--- www/sw.js
+++ www/sw.js
@@ -2,7 +2,7 @@
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-      caches.open('fxcalc-v1')
+      caches.open('broadcast-v1')
           .then(cache => cache.addAll([
               'index.html',
               'generic.html',
@@ -12,36 +12,19 @@ self.addEventListener('install', (event) => {
               '/js/main.js',
               '/js/skel.js',
               '/js/util.js',
-              'favicon.ico',
+              'favicon192.png',
+              'favicon512.png',
+              'favicon16.png',
               '/css/style.critical.css',
               '/css/font-awesome.css',
               'manifest.webmanifest',
-              '/image/pic01.webp',
-              '/image/pic02.webp',
-              '/image/pic03.webp',
-              '/image/pic04.webp',
-              '/image/pic05.webp',
-              '/image/pic06.webp',
-              '/image/pic07.webp',
-              '/image/pic08.webp',
-              '/image/pic09.webp',
-              '/image/pic10.webp',
-              '/image/pic11.webp',
-              '/image/bg.webp',
-              '/image/banner.webp',
-              '/fonts/FontAwesome.otf',
-              '/fontawsome-webfont.eot',
-              '/fontawsome-webfont.svg',
-              '/fontawsome-webfont.ttf',
-              '/fontawsome-webfont.woff',
-              '/fontawsome-webfont.woff2'
           ]))
   );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-      caches.open('fxcalc-v1').then((cache) => {
+      caches.open('broadcast-v1').then((cache) => {
           return fetch(event.request)
               .then((response) => {
                   cache.put(event.request, response.clone());
